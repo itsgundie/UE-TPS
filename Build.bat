148d89d804e07f433d13bfbf6d75f98218f73b2b
itsGundie <itsgundie@gmail.com>
2021-11-14 14:15:23 +0300

    init commit

---
+++ Build.bat
@@ -0,0 +1,7 @@
+"D:\Jenkins\Jobs\UE_4.27\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project=C:\Users\Gundie\Desktop\Unreal\JenkinsTPS\JenkinsTPS.uproject ^
+-platform=Win64 -clientconfig=Shipping ^
+-archivedirectory=C:\Users\Gundie\Desktop\Unreal\JenkinsTPS\BuildFromBat ^
+-build -cook -stage -stagingdirectory=C:\Users\Gundie\Desktop\Unreal\JenkinsTPS\Stage ^
+-archive -pak -package
